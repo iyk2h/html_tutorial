1e9d42b7c053195692955e0f1aff56ba6a12ebca
youngkyunIm <yee0230@naver.com>
2020-01-24 12:21:23 +0900

    good

---
+++ dya_night.js
@@ -0,0 +1,15 @@
+// function nightMode() {
+// return alert('Hello world');
+// }
+// function nightMode() {
+//     if(document.body.dataset.mode === 'night') {
+//         document.body.style.backgroundColor='white';
+//         document.body.style.color='black';
+
+//         document.body.dataset.mode = 'day';
+//     } else {
+//         document.body.style.backgroundColor='black';
+//         document.body.style.color='white';
+
+//         document.body.dataset.mode = 'night';
+//     }
